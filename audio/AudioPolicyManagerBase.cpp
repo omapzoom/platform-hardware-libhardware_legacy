02ea50cac13e3920995c3d2712c17f423b3d2573
Shitiz Kumar <shitiz_kumar@ti.com>
2011-11-22 18:06:29 -0600

    BT HF Tx: Modified the AudioPolicyManager for BT HF MM Streaming
    The patch changes the audio routing policy once the SCO connection is
    established. Once SCO connection gets established the media stream gets
    routed to BT SCO headset as per BT SCO MM use case.
    The patch is pulled in from commit b23f556b4e542eff5351609ba895177be861e3bc
    from gingerbread branch.
    Cherry-pick from ICS.
    
    Change-Id: Iea64d8e4eca1a0eadb526f290935c81156f30bf2
    Signed-off-by: Shitiz Kumar <shitiz_kumar@ti.com>

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -2378,6 +2378,21 @@ audio_devices_t AudioPolicyManagerBase::getDeviceForStrategy(routing_strategy st
                 (mForceUse[AudioSystem::FOR_DOCK] == AudioSystem::FORCE_ANALOG_DOCK)) {
             device2 = mAvailableOutputDevices & AUDIO_DEVICE_OUT_ANLG_DOCK_HEADSET;
         }
+#ifdef OMAP_ENHANCEMENT
+        // Once SCO connection is connected, map strategy_media to
+        // SCO headset for music streaming. BT SCO MM_UL use case
+        if (mForceUse[AudioSystem::FOR_COMMUNICATION] == AudioSystem::FORCE_BT_SCO) {
+           if (device2 == AUDIO_DEVICE_NONE) {
+                 device2 = mAvailableOutputDevices & AUDIO_DEVICE_OUT_BLUETOOTH_SCO_CARKIT;
+           }
+           if (device2 == AUDIO_DEVICE_NONE) {
+                 device2 = mAvailableOutputDevices & AUDIO_DEVICE_OUT_BLUETOOTH_SCO_HEADSET;
+           }
+           if (device2 == AUDIO_DEVICE_NONE) {
+                 device2 = mAvailableOutputDevices & AUDIO_DEVICE_OUT_BLUETOOTH_SCO;
+           }
+        }
+#endif
         if (device2 == AUDIO_DEVICE_NONE) {
             device2 = mAvailableOutputDevices & AUDIO_DEVICE_OUT_SPEAKER;
         }
