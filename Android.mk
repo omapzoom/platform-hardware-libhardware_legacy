fcd881079c0c1938dd1e8471e0207548fc893f58
Eyal Reizer <eyalr@ti.com>
2014-04-09 18:14:47 +0300

    wifi: support adding p2p0 when starting wpa_supplicant
    Add p2p0 using an NL80211 command when wpa_supplicant is started, and
    p2p_supported is enabled.
    
    [vishalm@ti.com: Added the changes inside ifdef]
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    
    [Ported to JB MR2 and squashed with following changes]
    
    [Vishal Mahaveer <vishalm@ti.com>]
        wifi: avoid crash when no phy's are found
    
    [modified for KK and squashed with following changes]
    
    [Barak Bercovitz <barak@wizery.com>]
        wifi: remove p2p interface on supplicant stop
    
    Change-Id: I2ccbae3cf00fb6e96a3de4443a3dafcc5376f9b3
    Signed-off-by: Eyal Reizer <eyalr@ti.com>

--- Android.mk
+++ Android.mk
@@ -13,6 +13,12 @@ LOCAL_SHARED_LIBRARIES := libcutils liblog libwpa_client
 
 LOCAL_INCLUDES += $(LOCAL_PATH)
 
+ifdef USES_TI_MAC80211
+LOCAL_STATIC_LIBRARIES := libnl_2
+LOCAL_C_INCLUDES += external/libnl-headers
+LOCAL_C_INCLUDES += hardware/ti/wlan/mac80211/wpa_supplicant_lib
+endif
+
 LOCAL_CFLAGS  += -DQEMU_HARDWARE
 QEMU_HARDWARE := true
 
